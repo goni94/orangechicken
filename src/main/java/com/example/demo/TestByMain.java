b65256b3641a555ddf2dd66d4bb9f5ef4ad77a06
You Sang Gon <ybt4794@naver.com>
2024-05-31 14:17:39 +0900

    test

--- src/main/java/com/example/demo/TestByMain.java
+++ src/main/java/com/example/demo/TestByMain.java
@@ -3,5 +3,6 @@ package com.example.demo;
 public class TestByMain {
     public static void main(String[] args) {
         System.out.println("메인 게시판 만드는중");
+        System.out.println("dd");
     }
 }
