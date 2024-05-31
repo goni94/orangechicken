a3b29897c5e557ff3d07f9956489bea8af801bad
You Sang Gon <ybt4794@naver.com>
2024-05-31 14:11:17 +0900

    test

--- src/main/java/com/example/demo/TestByLee.java
+++ src/main/java/com/example/demo/TestByLee.java
@@ -3,5 +3,6 @@ package com.example.demo;
 public class TestByLee {
     public static void main(String[] args) {
         System.out.println("lee 만드는중");
+        System.out.println("dd");
     }
 }
